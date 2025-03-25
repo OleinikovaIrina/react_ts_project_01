4ce42453ad1231bc6bc92f5c0ac18fd13f503d09
OleinikovaIrina <irenolgaruda@gmail.com>
2025-03-25 15:27:39 +0100

    add lesson 06

---
+++ src/lessons/Lesson06/Lesson06.tsx
@@ -0,0 +1,18 @@
+// /* eslint-disable prefer-const */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+
+//пример импорта по умолчанию (нужно использовать точное название и заключать его 
+//в фигурные скобки)
+import Button from "../../components/Button/Button";
+import { getAction, TRAFFIC_LIGHT } from "./object_types";
+
+function Lesson06() {
+  console.log(getAction(TRAFFIC_LIGHT.GREEN));
+  
+
+  return <Button />
+}
+
+//Экспорт по умолчанию для компонента Lesson06
+export default Lesson06;
