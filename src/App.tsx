7fdcf6c41584f2a4c11d0905dc885d6d9e74957a
OleinikovaIrina <irenolgaruda@gmail.com>
2025-03-25 21:32:01 +0100

    Add Homework6

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 // импорт компонента Lesson06 по умолчанию
 // при импорте по умолчанию можно использовать другое названия для компонента
 // import Task6 from './lessons/Lesson06/Lesson06';
-import Lesson06 from './lessons/Lesson06/Lesson06';
-
+// import Lesson06 from './lessons/Lesson06/Lesson06';
+import Homework06 from './homeworks/Homework06/Homework06'
 function App() {
 
   return (
     <>
       {/* <Task6 /> */}
-      <Lesson06 />
-      {/* <Homework06/> */}
+      {/* <Lesson06 /> */}
+       <Homework06/> 
     </>
   )
 }
